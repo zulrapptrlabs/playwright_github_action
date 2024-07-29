c41fc43c0c868d1f3e94fd6040124de9db49125d
zulrapptrlabs <zazri@rapptrlabs.com>
2024-07-30 00:41:59 +0800

    testing

--- tests/Homewave/Verify_login_button_in_sign_up_page.spec.js
+++ tests/Homewave/Verify_login_button_in_sign_up_page.spec.js
@@ -6,6 +6,6 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'Sign up' }).click();
   await page.getByRole('link', { name: 'Log in' }).click();
 
-  // Expect a title "to contain" a substring.
+  //  a title "to contain" a substring.
   await expect(page).toHaveTitle(/Log in/);
 });
