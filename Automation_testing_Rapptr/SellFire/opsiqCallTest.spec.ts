4f2fc23d0499a042386fc05d1122601b91828a3b
Kingsley Okoli <Kingsleyiokoli@gmail.com>
2024-08-05 15:31:59 -0400

    Updated opsiqCallTest

--- Automation_testing_Rapptr/SellFire/opsiqCallTest.spec.ts
+++ Automation_testing_Rapptr/SellFire/opsiqCallTest.spec.ts
@@ -1,6 +1,7 @@
 import { microphonePermission } from './Pages(Sellfire)/microphonePermission';
 import { test, expect, chromium } from '@playwright/test';
 import loginPage from './Pages(Sellfire)/loginPage';
+import dialerPage from './Pages(Sellfire)/dialerPage';
 
 
 test('Call test', async () => {
@@ -14,15 +15,18 @@ test('Call test', async () => {
 
   // Instantiate the loginPage class
   const login = new loginPage(page);
+  const dialer = new dialerPage(page)
 
   // Navigate to the login page and perform login
   await login.navigateTo();
   await login.enterUserName('aetest@rapptrlabs.com');
   await login.enterPassword('xIrYq1szG1sjR7J3TPjo');
   await login.clickLoginButton();
-  // await page.pause()
-  await page.locator('#root > div.sc-kBgaMi.jishmL > div.sc-iHbOXn.ihtWmE > div > div > div.sc-elToUV.jABGzl > div > div:nth-child(2) > div.sc-ezXQAu.pTeTY > div.sc-gJLCiq.kMUfjx > div.sc-kBizdN.gGaVww > div:nth-child(1) > div:nth-child(3) > div.sc-gVpmeh.gCLILR > div:nth-child(1) > button').first().click();
-  //await page.pause()
+
+  // Click on the call button in a lead card
+  await dialer.callButton();
+
+  // Go through the call flow
   await page.getByRole('button', { name: 'Hang up' }).click();
   await page.getByRole('button', { name: 'No Contact', exact: true }).click();
   await page.getByRole('button', { name: 'Log Other Call Result' }).click();
